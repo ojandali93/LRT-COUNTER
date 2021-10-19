79d0d537126c2796aee922b4208472c49f90f9b3
OmarJ <omarjandali93@gmail.com>
2021-10-18 21:24:31 -0700

    added context hook to the applicaiton

--- counter/src/App.js
+++ counter/src/App.js
@@ -1,13 +1,21 @@
+import React, { useState } from 'react'
 import Counter from './components/counter'
 import CounterHooks from './components/counterHooks'
+
+export const ThemeContext = React.createContext()
+
 function App() {
+  const [theme, setTheme] = useState('red')
   return ( // the () allows us to return multiple lines of code'
-    <>
+    <ThemeContext.Provider value={{backgroundColor: theme}}>
       counter
       <Counter initialCount={0}/>
       counter hooks
       <CounterHooks initialCount={5}/>
-    </>
+      <button onClick={() => setTheme(prevTheme => {
+        return prevTheme === 'red' ? 'blue' : 'red'
+      })}>Toggle Theme</button>
+    </ThemeContext.Provider>
   )
 }
 
--- counter/src/components/counterHooks.js
+++ counter/src/components/counterHooks.js
@@ -1,15 +1,15 @@
-import React, { useState } from 'react'
+import React, { useState, useContext } from 'react'
+import { ThemeContext } from '../App'
 
 export default function CounterHooks({ initialCount }){
     const [count, setCount] = useState(initialCount)
-
-
+    const style = useContext(ThemeContext)
 
     return(
         <div>
-            <button onClick={() => setCount(previousCount => previousCount - 1)}>+</button>
+            <button style={style} onClick={() => setCount(previousCount => previousCount - 1)}>+</button>
             <span>{count}</span> 
-            <button onClick={() => setCount(previousCount => previousCount + 1)}>+</button>
+            <button style={style} onClick={() => setCount(previousCount => previousCount + 1)}>+</button>
         </div>
     )
 }
